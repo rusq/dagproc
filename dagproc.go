b6c95d2872a5adac710b386489f4e57e9f111b1b
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2022-09-10 08:57:20 +1000

    split processing logic

--- dagproc.go
+++ dagproc.go
@@ -1,103 +1,100 @@
-package main
+package dagproc
 
 import (
 	"context"
 	"fmt"
 	"log"
 	"sync"
-	"time"
 
 	"github.com/yourbasic/graph"
 )
 
-func main() {
-	var Nodes = []Node{
-		{ID: "0", Delay: 1, ParentIDs: []string{}},
-		{ID: "1", Delay: 1, ParentIDs: []string{"0"}},
-		{ID: "2", Delay: 1, ParentIDs: []string{"1"}},
-		{ID: "3", Delay: 4, ParentIDs: []string{"1"}},
-		{ID: "4", Delay: 1, ParentIDs: []string{"2"}},
-		{ID: "5", Delay: 1, ParentIDs: []string{"4"}},
-		{ID: "6", Delay: 2, ParentIDs: []string{"2", "3"}},
-		{ID: "7", Delay: 1, ParentIDs: []string{"1", "3", "8"}},
-		{ID: "8", Delay: 1, ParentIDs: []string{"5", "6"}},
-	}
-	g := NewGraph(Nodes)
+type Node interface {
+	ID() string
+	ParentIDs() []string
+	Do() error
+}
 
+func Process(n []Node, workers int) {
+	g := NewGraph(n)
 	order, ok := graph.TopSort(g.g)
 	if !ok {
 		log.Fatal("poop")
 	}
 
-	var nodeC = make(chan *Node)
+	var vertexC = make(chan *vertex)
 	// start generator
 	go func() {
-		defer close(nodeC)
+		defer close(vertexC)
 		for _, idx := range order {
-			nodeC <- &g.Nodes[idx]
+			vertexC <- &g.v[idx]
 		}
 	}()
 
 	var wg sync.WaitGroup
-	wg.Add(4)
-	for i := 0; i < 4; i++ {
+	wg.Add(workers)
+	for i := 0; i < workers; i++ {
 		go func() {
-			worker(context.Background(), nodeC)
+			worker(context.Background(), vertexC)
 			wg.Done()
 		}()
 	}
 	wg.Wait()
 }
 
-type Node struct {
-	ID        string
-	Delay     int
-	ParentIDs []string
-
+type vertex struct {
+	n        Node
 	wg       sync.WaitGroup // waitgroup is prerequisites wait group
-	children []*Node        // addresses of all child nodes
+	children []*vertex      // addresses of all child nodes
 }
 
-func worker(ctx context.Context, nodeC <-chan *Node) {
+func worker(ctx context.Context, vertexC <-chan *vertex) {
 	for {
 		select {
 		case <-ctx.Done():
 			return
-		case node, more := <-nodeC:
+		case vertex, more := <-vertexC:
 			if !more {
 				return
 			}
-			node.wg.Wait()
-			fmt.Println("start:", node.ID)
-			<-time.After(time.Duration(node.Delay) * time.Second)
-			fmt.Println("finish:", node.ID)
-			for i := range node.children {
-				node.children[i].wg.Done() // mark this prerequisite as done
+			vertex.wg.Wait()
+			fmt.Println("start:", vertex.n.ID())
+			if err := vertex.n.Do(); err != nil {
+				log.Printf("node: %s, error: %s", vertex.n.ID(), err)
+			}
+			fmt.Println("finish:", vertex.n.ID())
+			for i := range vertex.children {
+				vertex.children[i].wg.Done() // mark this prerequisite as done
 			}
 		}
 	}
 }
 
 type Graph struct {
-	Nodes []Node
-	g     *graph.Mutable
+	v []vertex
+	g *graph.Mutable
 }
 
 func NewGraph(nodes []Node) Graph {
 	var idx = make(map[string]int, len(nodes))
 	for i := range nodes {
-		idx[nodes[i].ID] = i
+		idx[nodes[i].ID()] = i
 	}
+
+	var vertices = make([]vertex, len(nodes))
 
 	g := graph.New(len(nodes))
 	for i := range nodes {
-		fmt.Printf("node %d has %d parents\n", i, len(nodes[i].ParentIDs))
-		nodes[i].wg.Add(len(nodes[i].ParentIDs))
-		for _, parID := range nodes[i].ParentIDs {
+		vertices[i] = vertex{
+			n: nodes[i],
+		}
+		fmt.Printf("node %d has %d parents\n", i, len(nodes[i].ParentIDs()))
+		vertices[i].wg.Add(len(nodes[i].ParentIDs()))
+		for _, parID := range nodes[i].ParentIDs() {
 			idxPar := idx[parID]
 			g.Add(idxPar, i)
-			nodes[idxPar].children = append(nodes[idxPar].children, &nodes[i])
+			vertices[idxPar].children = append(vertices[idxPar].children, &vertices[i])
 		}
 	}
-	return Graph{Nodes: nodes, g: g}
+	return Graph{v: vertices, g: g}
 }
--- examples/simple/main.go
+++ examples/simple/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"time"
 
+	"github.com/rusq/dagproc"
+)
+
+var _ dagproc.Node = &Node{}
+
+type Node struct {
+	id        string
+	delay     int
+	parentIDs []string
+}
+
+func (n Node) ID() string {
+	return n.id
+}
+
+func (n Node) ParentIDs() []string {
+	return n.parentIDs
+}
+
+func (n Node) Do() error {
+	time.Sleep(time.Duration(n.delay) * time.Second)
+	return nil
+}
+
+func main() {
+	var nodes = []dagproc.Node{
+		Node{id: "0", delay: 1, parentIDs: []string{}},
+		Node{id: "1", delay: 1, parentIDs: []string{"0"}},
+		Node{id: "2", delay: 1, parentIDs: []string{"1"}},
+		Node{id: "3", delay: 4, parentIDs: []string{"1"}},
+		Node{id: "4", delay: 1, parentIDs: []string{"2"}},
+		Node{id: "5", delay: 1, parentIDs: []string{"4"}},
+		Node{id: "6", delay: 2, parentIDs: []string{"2", "3"}},
+		Node{id: "7", delay: 1, parentIDs: []string{"1", "3", "8"}},
+		Node{id: "8", delay: 1, parentIDs: []string{"5", "6"}},
+	}
+	dagproc.Process(nodes, 4)
 }
